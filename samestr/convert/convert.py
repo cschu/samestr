31786c4787474525798cb9c7d5e2de95a6f15c3e
Christian Schudoma <christian.schudoma@embl.de>
2025-10-17 13:11:04 +0200

    some more tweaks

--- samestr/convert/convert.py
+++ samestr/convert/convert.py
@@ -33,12 +33,15 @@ def convert_qual(qual_string):
 
 
 def pileup(bam_stream, gene_file, min_bq, min_mq, min_depth, outstream=sys.stdout):
-    bases = parse_positions(gene_file)
+    # bases = parse_positions(gene_file)
+    contigs = initialise_contigs(gene_file)
 
-    f_table = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    # f_table = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    # refs = {}
+    # freqs = []
     cur_rname = None
 
-    contig_bases = None
+    contig = None
     for line in bam_stream:
         _, _, rname, begin, mapq, cigar, _, _, _, seq, qual, *_ = line.strip().split("\t")
 
@@ -46,18 +49,31 @@ def pileup(bam_stream, gene_file, min_bq, min_mq, min_depth, outstream=sys.stdou
             continue
 
         if rname != cur_rname:
-            cur_rname = rname            
-            contig_bases = bases.get(rname)
+            cur_rname = rname
+            contig = contigs.get(rname)
+            
+            # contig_bases = bases.get(rname)
+            # if contig_bases is not None:
+            #     start, end = contig_bases
+            #     rindex = refs.setdefault(rname, len(refs))
+            #     if rindex >= len(freqs):
+            #         freqs.append(np.zeros([1, end, 4]))
+            #     # f_table.setdefault(rname, np.zeros([1, end, 4]))
 
-        if not contig_bases:
+        # if contig_bases is None:
+        if contig is None:
             continue
-        start, end = contig_bases
+        # start, end = contig_bases
 
         p = int(begin)
         
         aln_string = decode_cigar(cigar)
         
-        bases_and_quals = iter(zip(seq, convert_qual(qual)))
+        bases_and_quals = iter(
+            zip(
+                ("ACGTN".index(b) for b in seq),
+                convert_qual(qual))
+        )
         
         for oplen, cigar_op in aln_string:
             if cigar_op == "H":
@@ -69,21 +85,27 @@ def pileup(bam_stream, gene_file, min_bq, min_mq, min_depth, outstream=sys.stdou
                 for pp in range(p, p + oplen):
                     cur_base, cur_qual = next(bases_and_quals)
                     if not is_insertion:
-                        base = (cur_base, "-")[(cur_qual < min_bq)]
-                        if base != "-" and start <= pp <= end:
-                            f_table[rname][pp][base] += 1
+                        # base = (cur_base, "-")[(cur_qual < min_bq)]
+                        # if base != "-" and start <= pp <= end:
+                        if cur_qual >= min_bq and cur_base < 4 and 1 <= pp <= contig.shape[1]:
+                            # f_table[rname][pp][base] += 1
+                            contig[0, pp - 1, cur_base] += 1
+                            # f_table[rname][0, pp - 1, cur_base] += 1
                 
                 if is_insertion:
                     continue
             
             p += oplen
 
-    for c, positions in f_table.items():
-        for pos, nucs in sorted(positions.items(), key=lambda x:x[0]):
-            for nuc, counts in sorted(nucs.items(), key=lambda x:x[0]):
-                if counts >= min_depth:
-                    print(c, pos, nuc, counts, sep="\t", file=outstream)
-                    yield c, pos, nuc, counts
+    for contig_name, contig in contigs.items():
+        contig[contig < min_depth] = 0
+        yield contig_name, contig
+    # for c, positions in f_table.items():
+    #     for pos, nucs in sorted(positions.items(), key=lambda x:x[0]):
+    #         for nuc, counts in sorted(nucs.items(), key=lambda x:x[0]):
+    #             if counts >= min_depth:
+    #                 print(c, pos, nuc, counts, sep="\t", file=outstream)
+    #                 yield c, pos, nuc, counts
 
 
 BASES = {b: i for b, i in zip('ACGT', range(4))}
@@ -122,8 +144,9 @@ def kp2np(kpileups, contig_map, sample, gene_file, output_dir):
     output_dir = pathlib.Path(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    x = initialise_contigs(gene_file)
-    add_pileups(kpileups, x)
+    # x = initialise_contigs(gene_file)
+    # add_pileups(kpileups, x)
+    x = {contig_id: pileups for contig_id, pileups in kpileups}
     cmap = read_contig_map(contig_map)
 
     # Concatenate contigs
