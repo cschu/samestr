a333955b2a14c9bb5812c510c5a986bd8c26dc63
Christian Schudoma <christian.schudoma@embl.de>
2025-10-20 21:46:22 +0200

    more optimisation

--- samestr/convert/convert.py
+++ samestr/convert/convert.py
@@ -32,8 +32,8 @@ def initialise_contigs_db(clades, db):
     contigs = {}
 
     # with sqlite3.connect(f"file:{db}?mode=ro", uri=True) as conn:
-    with sqlite3.connect(f"file:{db}?mode=ro", uri=True) as conn:  # source, sqlite3.connect(':memory:') as conn:
-        #source.backup(conn)
+    with sqlite3.connect(f"file:{db}?mode=ro", uri=True) as source, sqlite3.connect(':memory:') as conn:
+        source.backup(conn)
         cursor = conn.execute(
             f"SELECT clade.id, clade.name FROM clade WHERE clade.name IN ({query_placeholders})",
             clades
